87d60e4c4a1a0ce22f45276901793427b5a69de0
luukb <l.b.bartels@student.tudelft.nl>
2023-04-11 16:32:53 +0200

    Aangepaste code voor spring3

--- evaluate.py
+++ evaluate.py
@@ -142,8 +142,9 @@ def validate_spring(model, iters=32, max_its=-1):
 
     # @Luuk, spring heeft dus maar een enkele set, tenzij we left en right willen doen, in dat geval zou je hier bijna
     # bijna niets meer hoeven aanpassen
-    for dstype in ['0001/flow_FW_left']:
+    for dstype in ['0001/frame_left']:
         val_dataset = datasets.EvalSpring(split='train', dstype=dstype)
+        #val_dataset = datasets.EvalSpring(dstype=dstype)
 
         # Ik heb dit toegevoegd om snel te kunnen checken of de evaluation runt, als je een max_its param mee geeft
         # limit hij het aantal iteraties van de evaluation
